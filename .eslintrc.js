49286cdc65f6cb6f2d5b9e227ad92bd310f87e41
Stefano Christian Wiryana <stefanocwiryana@gmail.com>
2023-06-09 14:13:05 +0700

    Set up husky, prettier, and lint-staged

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: { browser: true, es2021: true },
+  extends: [
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['prettier'],
+};
